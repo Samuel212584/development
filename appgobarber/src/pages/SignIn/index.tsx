ec93b798ea8d855b01f3ad3f7f354a124e8e60b2
Samuel212584 <samuel212584@gmail.com>
2020-08-01 15:31:09 -0300

    Auth hook and SignIn authentication

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
 
+import { useAuth } from '../../hooks/auth';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -39,7 +41,12 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
+
   const navigation = useNavigation();
+
+  const { signIn, user } = useAuth();
+
+  console.log(user);
 
   const handleSignIn = useCallback(
     async (data: SignInFormData) => {
@@ -57,12 +64,10 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        // await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // });
-
-        // history.push('/dashboard');
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -78,7 +83,9 @@ const SignIn: React.FC = () => {
         );
 
       }
-    }, []);
+    },
+    [signIn],
+  );
 
   return (
     <>
--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -83,7 +83,9 @@ const SignUp: React.FC = () => {
           'Ocorreu um erro ao fazer cadastro, tente novamente!',
         );
       }
-    }, []);
+    },
+    [navigation],
+  );
 
   return (
     <>
