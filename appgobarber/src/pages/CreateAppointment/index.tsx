4fc2550e2c8187f6adf778d6d601cfcaf9b6f4d2
Samuel212584 <samuel212584@gmail.com>
2020-09-21 16:23:22 -0300

    Create appointment page header

--- appgobarber/src/pages/CreateAppointment/index.tsx
+++ appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const Dashboard: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+  const { goBack } = useNavigation();
+
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+
+  const handleGoBackButton = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={handleGoBackButton}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+
+          <HeaderTitle>Cabeleireiros</HeaderTitle>
+        </BackButton>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
